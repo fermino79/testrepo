1861f1ad686ca19d4c97803fabfd0b24cc78f865
fermino79 <90787582+fermino79@users.noreply.github.com>
2021-10-25 10:05:32 -0500

    Update firstpython.py

--- firstpython.py
+++ firstpython.py
@@ -3,12 +3,6 @@
 cd /home/project
 
 theia@theiadocker-fernandomin1:/home/project$ [ ! -d 'xzceb-flask_eng_fr' ] && git clone https://github.com/fermino79/testrepo.git
-Cloning into 'testrepo'...
-remote: Enumerating objects: 10, done.
-remote: Counting objects: 100% (10/10), done.
-remote: Compressing objects: 100% (8/8), done.
-remote: Total 10 (delta 0), reused 0 (delta 0), pack-reused 0
-Unpacking objects: 100% (10/10), done.
 
 cd /home/project/xzceb-flask_eng_fr/final_project
 
@@ -24,3 +18,35 @@ pip3 install flask ibm-watson python-dotenv
 apikey=cpQ_3wwMej4ir2nGhuJyigEp36ajy05d5ua5ZpeecvbI
 url=https://api.us-south.language-translator.watson.cloud.ibm.com/instances/c78a13ac-5e0f-4bc1-979e-0b93d8e076b0
   
+#In the explorer, go to the machinetranslation directory and create a new file called translator.py. Enter the following lines of code.#
+import json
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+#Lines of code#
+import json
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+
+authenticator = IAMAuthenticator('{apikey}')
+language_translator = LanguageTranslatorV3(
+    version='2018-05-01',
+    authenticator=authenticator
+)
+
+language_translator.set_service_url('{url}')
+
+translation = language_translator.translate(
+    text='Hello, how are you today?',
+    model_id='en-es').get_result()
+print(json.dumps(translation, indent=2, ensure_ascii=False))
+
+apikey = os.environ['apikey']
+url = os.environ['url']
+  
+#Add code to create an instance of the IBM Watson Language translator in translator.py. > Hint : You may refer to the IBM Watson Language Translation API documents here#
+
